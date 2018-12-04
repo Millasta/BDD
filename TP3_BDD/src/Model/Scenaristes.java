a28f880eebf085be9d6f01841bae5dd207cdb2a4
mberv <mberv@DESKTOP-D87Q0EI>
2018-12-03 19:19:30 -0500

    Javadoc Scenaristes/ScenaristesId

--- TP3_BDD/src/Model/Scenaristes.java
+++ TP3_BDD/src/Model/Scenaristes.java
@@ -4,22 +4,40 @@ public class Scenaristes implements java.io.Serializable {
 
 	private static final long serialVersionUID = -5470530741718287305L;
 	
+	/**
+	 * Attributs de la classe correspondant aux champs de la table
+	 */
 	private ScenaristesId id;
 
+	/**
+	 * Constructeur par defaut
+	 */
 	public Scenaristes()
 	{
 	}
 	
+	/**
+	 * Construit un scenariste avec son id
+	 * @param id : l'id du scenariste
+	 */
 	public Scenaristes(ScenaristesId id)
 	{
 		this.id = id;
 	}
 
+	/**
+	 * Getter de l'id
+	 * @return ScenaristesId id
+	 */
 	public ScenaristesId getId()
 	{
 		return id;
 	}
 
+	/**
+	 * Setter de l'id
+	 * @param id : le nouvel id
+	 */
 	public void setId(ScenaristesId id)
 	{
 		this.id = id;
--- TP3_BDD/src/Model/ScenaristesId.java
+++ TP3_BDD/src/Model/ScenaristesId.java
@@ -4,35 +4,61 @@ public class ScenaristesId implements java.io.Serializable {
 
 	private static final long serialVersionUID = 2048149638108548137L;
 
+	/**
+	 * Attributs de la classe correspondant aux champs de la table
+	 */
 	private Personnes personnes;
 	private Films films;
 	
+	/**
+	 * Constructeur par defaut
+	 */
 	public ScenaristesId()
 	{
 	}
 	
+	/**
+	 * Construit l'id d'un scenariste avec la personne et le film correspondant
+	 * @param personnes : la personne correspondante
+	 * @param films : le film correspondant
+	 */
 	public ScenaristesId(Personnes personnes, Films films)
 	{
 		this.personnes = personnes;
 		this.films = films;
 	}
 	
+	/**
+	 * Getter de la personne
+	 * @return Personnes personnes
+	 */
 	public Personnes getPersonnes()
 	{
 		return personnes;
 	}
 
+	/**
+	 * Setter de la personnes
+	 * @param personnes : la nouvelle personne
+	 */
 	public void setPersonnes(Personnes personnes)
 	{
 		this.personnes = personnes;
 	}
 
-	
+	/**
+	 * Getter du film
+	 * @return Films films
+	 */
 	public Films getFilms()
 	{
 		return films;
 	}
 
+	/**
+	 * Setter du film
+	 * @param films : le nouveau film
+	 */
 	public void setFilms(Films films)
 	{
 		this.films = films;
